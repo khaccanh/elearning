d261048107d6b06cb43a1a8b1b37245d158cf11c
khaccanh <khaccanh@gmail.com>
2020-12-26 11:50:16 +0700

    Add files via upload

---
+++ brain/assets/mail/run_code.js
@@ -0,0 +1,13 @@
+function reloadRunCode() {
+    var iframeController = document.getElementById("iframe_run_code")
+    iframeController.src = ""
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var objJson = JSON.parse(this.response)
+            iframeController.srcdoc = objJson.phaser
+        }
+    };
+    xhttp.open("GET", "https://128.199.168.250:8100/api/CodeLuas/142", true);
+    xhttp.send()
+}
