584327a2212ee51296a342dbb42344e39515c749
khaccanh <khaccanh@gmail.com>
2020-12-30 14:33:27 +0700

    Add files via upload

--- brain/assets/mail/run_code.js
+++ brain/assets/mail/run_code.js
@@ -1,3 +1,4 @@
+var indexCode = 145
 function reloadRunCode() {
     var iframeController = document.getElementById("iframe_run_code")
     iframeController.src = ""
@@ -8,6 +9,14 @@ function reloadRunCode() {
             iframeController.srcdoc = objJson.phaser
         }
     };
-    xhttp.open("GET", "https://128.199.168.250:8100/api/CodeLuas/145", true);
+    xhttp.open("GET", "https://128.199.168.250:8100/api/CodeLuas/" + indexCode, true);
     xhttp.send()
+}
+
+function downloadCode() {
+    indexCode = indexCode + 1
+    if (indexCode > 145) {
+        indexCode = 143
+    }
+    console.log(indexCode)
 }
